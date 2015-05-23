29b4b140d082a869785c22f0b099ef1b1883f080
Yu Hoshino <yhoshino11@gmail.com>
2015-05-23 15:16:58 +0900

    Sample program

---
+++ 2-1-1/sample.py
@@ -0,0 +1,14 @@
+def main():
+    """
+    Main function for sample program.
+    """
+
+    for i in range(1, 6):
+        if i % 2 == 0:
+            print("%s is even." % i)
+        else:
+            print("%s is odd." % i)
+
+
+if __name__ == "__main__":
+    main()
