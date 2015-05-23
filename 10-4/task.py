1826eda8ef70e8e5b25915991282f794e1d0afef
Yu Hoshino <yhoshino11@gmail.com>
2015-05-24 04:20:40 +0900

    CLI Finished

--- 10-4/task.py
+++ 10-4/task.py
@@ -1,4 +1,4 @@
-#!/Users/yuhoshino/.pyenv/shims/python3
+#!/Users/yuhoshino/.pyenv/shims/python
 # encoding: utf-8
 from datetime import datetime, time
 
@@ -62,6 +62,68 @@ def unfinished_tasks(db):
             if not task['finished'])
 
 
+import argparse
+
+
+def cmd_add(args):
+    name = raw_input('task name:')
+    due_date = datetime.strptime(raw_input('due date [Y-m-d]:'), '%Y-%m-%d')
+    required_time = int(input('required_time:'))
+
+    task = create_task(name, due_date, required_time)
+    add_task(args.db, task)
+
+
+def cmd_list(args):
+    if args.all:
+        tasks = all_task(args.db)
+    else:
+        tasks = unfinished_tasks(args.db)
+
+    for key, task in tasks:
+        print("{0} {1}".format(key, format_task(task)))
+
+
+def cmd_finish(args):
+    task = args.db[args.task]
+    finish_task(task)
+    args.db[args.task] = task
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('shelve')
+
+    subparsers = parser.add_subparsers()
+
+    add_parser = subparsers.add_parser('add')
+    add_parser.set_defaults(func=cmd_add)
+
+    list_parser = subparsers.add_parser('list')
+    list_parser.add_argument('-a', '--all', action='store_true')
+    list_parser.set_defaults(func=cmd_list)
+
+    finish_parser = subparsers.add_parser('finish')
+    finish_parser.add_argument('task')
+    finish_parser.set_defaults(func=cmd_finish)
+
+    args = parser.parse_args()
+
+    db = shelve.open(args.shelve, 'c')
+
+    try:
+        args.db = db
+        if hasattr(args, 'func'):
+            args.func(args)
+        else:
+            parser.print_help()
+    finally:
+        db.close()
+
+
+if __name__ == '__main__':
+    main()
+
 # How to use
 
 # from task import *
@@ -81,3 +143,11 @@ def unfinished_tasks(db):
 # unfinished_tasks(db
 
 # next_task_name(db)
+
+
+# CLI
+# ./task.py task_data add
+# ./task.py task_data list
+# ./task.py task_data finish task:0
+# ./task.py task_data list # hides finished tasks
+# ./task.py task_data list -a # shows all tasks including finished tasks
