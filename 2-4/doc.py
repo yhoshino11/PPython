5320d3b00d94547188fc8b868adbaad40357ae68
Yu Hoshino <yhoshino11@gmail.com>
2015-05-23 15:30:19 +0900

    Docstring test with multiple line documents.

--- 2-4/doc.py
+++ 2-4/doc.py
@@ -1,5 +1,8 @@
 def docstring_test():
-    """This is a docstring test."""
+    """
+    This is a docstring test.
+    This is multiple lines docstring test.
+    """
     return True
 
 print(docstring_test.__doc__)
